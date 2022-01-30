671469074b5e0ee78fd5a1f657c674b410f653d1
Nasser Nasser <nhani.nasser@gmail.com>
2022-01-30 18:08:26 -0500

    finished adding all the questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,26 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const { title } = require('process');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "inout",
+        name: "projectTitle",
+        message: "What is the title of your project"
+    },
+    {
+        tyoe: "input",
+        name: "prjoectDescription",
+        message: "Describe your project",
+    },
+    {
+        type: "input",
+        name: "projectInstallation",
+        message: "Describe how to use your project",
+    },
+    {
+       
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
