6d5baa4bea5502465b66524de49af55276f0977c
Nasser Nasser <nhani.nasser@gmail.com>
2022-01-30 22:01:30 -0500

    added and made the function to render license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseRender = Number(license);
+  switch (licenseRender)  {
+    case 1: {
+      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    };
+      break;
+    case 2: {
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    };
+      break;
+    case 3: {
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    };
+      break;
+    case 4: {
+      return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+    };
+      break;
+    case 5: {
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    };
+      break;
+    default: {
+      return ''
+    };
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
