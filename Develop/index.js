7ffd87355ef848c832fe07ef637866ee9ef002c6
Nasser Nasser <nhani.nasser@gmail.com>
2022-01-30 19:30:44 -0500

    new file generator function

--- Develop/index.js
+++ Develop/index.js
@@ -65,10 +65,26 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then(({
+        projectTitle,
+        projectDescription,
+        projectInstallation,
+        projectUsage,
+        projectLicense,
+        projectContributors,
+        projectTests,
+        projectIssues,
+        userGithub,
+        userEmail
+    }) => {
+}
 
 // Function call to initialize app
 init();
