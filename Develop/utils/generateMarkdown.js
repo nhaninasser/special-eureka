a4dcdcb6157cb94347a032ed672512a8f23d88a7
Nasser Nasser <nhani.nasser@gmail.com>
2022-01-30 22:12:38 -0500

    made a function that renders the license links

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -31,11 +31,40 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  function renderLicenseLink(license) {
+    const licenseRender = Number(license);
+    switch (licenseRender) {
+      case 1: {
+        return 'https://opensource.org/licenses/Apache-2.0'
+      };
+        break;
+      case 2: {
+        return 'https://www.gnu.org/licenses/gpl-3.0'
+      };
+        break;
+      case 3: {
+        return 'https://opensource.org/licenses/MIT'
+      };
+        break;
+      case 4: {
+        return 'https://opensource.org/licenses/ISC'
+      };
+        break;
+      case 5: {
+        return 'https://opensource.org/licenses/MPL-2.0'
+      };
+      break;
+      default: {
+        return ''
+      };
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
